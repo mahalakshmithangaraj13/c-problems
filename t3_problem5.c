a6f8efbfd44397fc6fb532fe4cc5ae620b6e4a66
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-02-07 06:48:56 +0530

    Add files via upload

---
+++ t3_problem5.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    float a;
+    a=sizeof(a);
+    printf("%.f",a);
+    return 0;
+}
---
+++ t3_problem7.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    double a;
+    a=sizeof(a);
+    printf("%.lf",a);
+    return 0;
+}
