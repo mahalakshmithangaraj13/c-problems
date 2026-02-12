cab381d01d2df4465160699d075d266a519f4a34
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-02-12 19:47:55 +0530

    Modify nested loop to print pattern based on input n

--- nested loop/problem4.c
+++ nested loop/problem4.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+ #include<stdio.h>
 int main(){
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=10;i++){   
-        {
-         printf("%d ",n*i);}
+    for(int i=1;i<=n;i++){
+        for (int j=1;j<i+1;j++){
+            printf("%d",i);
+        }
+        printf("\n");
     }
     return 0;
 }
