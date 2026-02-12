957be7ef01f42cf63fb721ca0b8422b771cd8b20
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-02-12 19:47:05 +0530

    Refactor to print asterisks in nested loops

--- nested loop/problem2.c
+++ nested loop/problem2.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+//*
+//**
+//***
+//****
+//*****
 int main(){
-    int n,sum=0;
+    int n;
     scanf("%d",&n);
-    for (int i=1;i<=n;i++){
-        sum=sum+i;   
+    for(int i=1;i<=n;i++){
+        for (int j=1;j<i+1;j++){
+            printf("*");
+        }
+        printf("\n");
     }
-    printf("%d ",sum);
     return 0;
 }
