7d8a6eb6e924ba6a7435fabfaeb60366b5be8ddd
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-02-13 19:13:34 +0530

    Add files via upload

---
+++ nested loop/pattern15.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i=n;i>0;i--){
+        char alphabet='A';
+        for(int j=1;j<=i;j++){
+            printf("%c",alphabet);
+        alphabet++;
+        }
+    printf("\n");
+    }
+    return 0;
+}
