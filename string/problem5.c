f6152132ef0bc6d3c038f78a7fd9897dafc45811
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 06:42:00 +0530

    Palindrome Product Code

--- string/problem5.c
+++ string/problem5.c
@@ -1,38 +1,22 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int rows,colums;
-    scanf("%d %d",&rows,&colums);
-    int arr[rows][colums];
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<colums;j++){
-            scanf("%d",&arr[i][j]);
+    char str[20];
+    int i,palindrome=1;
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    int len =strlen(str);
+    for(i=0;i<len/2;i++){
+        if(str[i]!=str[len-i-1]){
+            palindrome=0;
+            break;
         }
     }
-    int sum,arr1[rows];
-    for(int i=0;i<rows;i++){
-        sum=0;
-        for(int j=0;j<colums;j++){
-            sum+=arr[i][j];
-        }
-        arr1[i]=sum;
+    if(palindrome){
+        printf("Palindrome");
     }
-     int avg,arr2[rows];
-     for(int i=0;i<rows;i++){
-        for(int j=0;j<colums;j++){
-            avg=arr1[i]/colums;
-        }
-        arr2[rows]=avg;
-     }
-     int minavg=arr2[0],index=0;
-     for(int i=1;i<rows;i++){
-        if(arr2[i]<minavg){
-            minavg=arr2[i];
-            index=i;
-        }
-        else{
-            index=0;
-        }
-     }
-     printf("%d",index);
-     return 0;
+    else{
+        printf("Not a Palindrome");
+    }
+    return 0;
 }
