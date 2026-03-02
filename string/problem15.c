218ebd9af28628296e05f86da0c62ea010dbb369
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 10:08:02 +0530

    Word Counter in Text Editor

---
+++ string/problem15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[20];
+    int count=0;
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==' '){
+           count++;
+
+        }
+    }
+    printf("%d",count+1);
+    return 0;
+
+}
