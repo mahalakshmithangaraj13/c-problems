3d501240505fcdb251a6a5ab1bd3c861b52a5b51
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 06:40:55 +0530

    Reverse a Customer Feedback

--- string/problem4.c
+++ string/problem4.c
@@ -1,29 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int rows,columns;
-    scanf("%d %d",&rows,&columns);
-    int arr[rows][columns];
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-    scanf("%d",&arr[i][j]);
-        }
+    char str[20];
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    int len =strlen(str);
+    for(int i=len-1;i>=0;i--){
+        printf("%c",str[i]);
     }
-    int count,element=0,maxcount=0;
-    for(int k=0;k<rows;k++){
-        for(int l=0;l<columns;l++){
-            count=0;
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-            if(arr[k][l]==arr[i][j]){
-                 count++;
-            }
-        }
-    if(maxcount<count){
-    maxcount=count;
-    element=arr[k][l];}
-    }
-        }
-    }
-    printf("%d",element);
     return 0;
 }
