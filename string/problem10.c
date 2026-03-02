2652d9e5775b5b2819da8f13f5ebc3606681899e
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 06:37:40 +0530

    Uppercase Check

--- string/problem10.c
+++ string/problem10.c
@@ -1,24 +1,15 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int rows,columns;
-    scanf("%d %d",&rows,&columns);
-    int arr[rows][columns];
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-            scanf("%d",&arr[i][j]);
+    char str[20];
+    int count=0;
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='A' && str[i]<='Z'){
+            str[i]=str[i]+32;
         }
     }
-    int product,arr1[rows];
-    int high=0,index=0;
-    for(int i=0;i<rows;i++){
-        product=1;
-        for(int j=0;j<columns;j++){
-            product*=arr[i][j];
-        }
-        if(product>high){
-           high=product;
-           index=i;
-        }
-}
-    printf("%d",index);
+    printf("%s",str);
+    return 0;
 }
