28d64145293f7bd009ec77cd2ccd2a9fa5892a8f
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 06:38:41 +0530

    Email Username Extractor

--- string/problem2.c
+++ string/problem2.c
@@ -1,26 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int rows,colums;
-    scanf("%d %d",&rows,&colums);
-    int arr[rows][colums];
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<colums;j++){
-            scanf("%d",&arr[i][j]);
+    char str[20];
+    int uppercase=0;
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='A' && str[i]<='Z'){
+            uppercase=1;
+            break;
         }
     }
-    int product, max=0;
-    for(int i=0;i<rows;i++){
-        for(int k=i+1;k<rows;k++){
-        for(int j=0;j<colums;j++){
-            for(int l=0;l<colums;l++){
-                product=arr[i][j]*arr[k][l];
-            if(product>max){
-                max=product;
-            }
-        }
-        }   
-    }        
+    if(uppercase){
+        printf("Valid password\n");
     }
-    printf("%d",max);
+    else{
+        printf("Invalid password");
+    }
     return 0;
 }
