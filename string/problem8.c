f765c8e30705eb63cac645e7c05bbaf9ccc0808e
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 06:44:26 +0530

    Space Counter in Delivery Address

--- string/problem8.c
+++ string/problem8.c
@@ -1,22 +1,15 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int rows,colums;
-    scanf("%d %d",&rows,&colums);
-    int arr[rows][colums];
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<colums;j++){
-            scanf("%d",&arr[i][j]);
+    char str[30];
+    int count=0;
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==' '){
+            count++;
         }
     }
-    int max=0;
-    for(int i=0;i<rows-1;i++){
-        int sum=0;
-        for(int j=0;j<colums-1;j++){
-            sum=arr[i][j]+arr[i+1][j]+arr[i][j+1]+arr[i+1][j+1];  
-        
-     if(sum>max){
-        max=sum;
-            }  
-        }}
-    printf("%d",max);
+    printf("%d",count);
+    return 0;
 }
