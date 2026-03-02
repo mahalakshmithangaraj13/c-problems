19afcdfecb885a8c7ab9b962efb23db4d2a6d742
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 06:42:44 +0530

    Vowel Counter in Reviews
    Replaced the original matrix sum calculation with a vowel counting program.

--- string/problem6.c
+++ string/problem6.c
@@ -1,34 +1,15 @@
 #include<stdio.h>
+#include<stdio.h>
+#include<string.h>
 int main(){
-    int rows,colums;
-    scanf("%d %d",&rows,&colums);
-    int arr[rows][colums];
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<colums;j++){
-            scanf("%d",&arr[i][j]);
-        }
+    char str[20];
+    int count =0;
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'||str[i]=='a'||
+        str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+        count++;}
     }
-    int sum,arr1[rows];
-    for(int i=0;i<rows;i++){
-        sum=0;
-    for(int j=0;j<colums;j++){
-        sum+=arr[i][j];
-    }
-    arr1[i]=sum;
-    }
-    int high=0,diff;
-    for(int i=0;i<rows;i++){
-        for(int j=i+1;j<rows;j++){
-        diff=arr1[i]-arr1[i+1];
-    
-        if(diff<0){
-            diff=-diff;
-        }
-    if(diff>high){
-        high=diff;
-    }
-}
-    }
- printf("%d",high);
-
+    printf("%d",count);
 }
