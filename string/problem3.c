cf923ddb34daeacf6fe90149ec5fda7387d72af8
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 06:39:57 +0530

    Email Username Extractor

--- string/problem3.c
+++ string/problem3.c
@@ -1,26 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int rows,colums;
-    scanf("%d %d",&rows,&colums);
-    int arr[rows][colums];
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<colums;j++){
-            scanf("%d",&arr[i][j]);
-        }
+    char str[20];
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    for(int i=0;str[i]!='@';i++){
+        printf("%c",str[i]);
     }
-    int isunique,count=0;
-    for(int i=0;i<rows;i++){
-        isunique=1;
-        for(int j=0;j<colums;j++){
-            if(arr[i][j]==arr[i][j+1]){
-                isunique=0;
-                break;
-            }
-        }
-        if(isunique==1){
-            count++;
-        }
-    }
-    printf("%d",count);
     return 0;
 }
