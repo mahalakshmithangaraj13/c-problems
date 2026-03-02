65d9776f4152267d7936b90f58dd7a7af72fcf79
Mahalakshmi Thangaraj <mahaapunithaa@gmail.com>
2026-03-02 13:58:51 +0530

    Factorial Using Function

---
+++ functions/problem6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[20];
+    int count =0;
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]='\0';
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'||str[i]=='a'||
+        str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+        count++;}
+    }
+    printf("%d",count);
+}
